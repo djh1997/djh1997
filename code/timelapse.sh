77b7b94f1c6708c3fc60636adb2e266ad5c2606a
jo <djh1997@hotmail.com>
2023-12-20 14:10:38 +0000

    add old useful bits of code

---
+++ code/timelapse.sh
@@ -0,0 +1,2 @@
+ffmpeg -r 30 -start_number 9086 -i IMG_%04d.JPG -s hd480 -vcodec libx264 time-lapse.mp4
+
---
+++ code/wget.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+wget -r -l1 -H -t1 -nd -N -np -A.torrent -erobots=off https://tails.boum.org/torrents/files/
+wget -r -l1 -H -t1 -nd -N -np -A.torrent -erobots=off https://ubuntu.com/download/alternative-downloads
+wget -r -l1 -H -t1 -nd -N -np -A.torrent -erobots=off https://www.raspberrypi.org/downloads/noobs/
+wget -r -l1 -H -t1 -nd -N -np -A.torrent -erobots=off https://www.raspberrypi.org/downloads/raspberry-pi-os/
+wget -r -l1 -H -t1 -nd -N -np -A.torrent -erobots=off https://www.raspberrypi.org/downloads/raspberry-pi-desktop/
+wget -r -l1 -H -t1 -nd -N -np -A.torrent -erobots=off https://ubuntu-mate.org/download/all/
+#cd multi
+#wget -r -l2 -H -t1 -nd -N -np -A.torrent -erobots=off https://www.raspberrypi.org/downloads/
+#wget -r -l2 -H -t1 -nd -N -np -A.torrent -erobots=off https://www.kali.org/downloads/
