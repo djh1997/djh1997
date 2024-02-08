6b5f373c77a44b8f8cae58b4d4fcc75b6a13e562
jo <djh1997@hotmail.com>
2024-02-08 20:36:49 +0000

    add  install

---
+++ code/install.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install htop nginx cert-bot vlc
+wget https://gist.githubusercontent.com/geerlingguy/91d4736afe9321cbfc1062165188dda4/raw/f6cbb1c540405fc677e8f530d878f72d7b7dd226/pi-cpu-stress.sh
