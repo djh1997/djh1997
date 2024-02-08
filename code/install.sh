bd2ab24dc8b7ce76898421919a1a19ede9af5611
jo <djh1997@hotmail.com>
2024-02-08 20:55:05 +0000

    correct certbot and add iperf3

--- code/install.sh
+++ code/install.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install htop nginx cert-bot vlc
+sudo apt-get install htop nginx certbot vlc iperf3
 wget https://gist.githubusercontent.com/geerlingguy/91d4736afe9321cbfc1062165188dda4/raw/f6cbb1c540405fc677e8f530d878f72d7b7dd226/pi-cpu-stress.sh
