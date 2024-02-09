3992d8b2c72071de8b52f87629c61d39f496d4db
jo <djh1997@hotmail.com>
2024-02-09 12:52:14 +0000

    add screen

--- code/install.sh
+++ code/install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install htop nginx certbot vlc iperf3 git code
+sudo apt-get install htop nginx certbot vlc iperf3 git code screen
 wget https://gist.githubusercontent.com/geerlingguy/91d4736afe9321cbfc1062165188dda4/raw/f6cbb1c540405fc677e8f530d878f72d7b7dd226/pi-cpu-stress.sh
 curl -L https://install.pivpn.io | bash
