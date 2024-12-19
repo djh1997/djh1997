e8a20d51a30bfef6b086f3b9ceed5eb1d317cc85
jo <djh1997@hotmail.com>
2024-12-19 11:22:11 +0000

    Update imposter.py
    Correct

--- code/imposter.py
+++ code/imposter.py
@@ -1,23 +1,26 @@
+from random import randint
 itemf = open("items.txt","r")
 itemlist = itemf.read().split("\n")
 itemf.close()
-
-item = itemlist[randnum(0,len(itemlist))]
+randitemnum=randint(0,len(itemlist)-1)
+item = itemlist[randitemnum]
 
 numf = open("numbers.txt", "r")
 numlist= numf.read().split()
 numf.close()
 
-master = randnum(0,len(numlist))
-outlist[0] = [numlist.pop(master),item]
+master = randint(0,len(numlist)-1)
+outlist = []
+outlist.append([numlist.pop(master),' master ',item,'\n'])
 
-imposter = randnum(0,len(numlist))
-outlist[1] = [numlist.pop(imposter),item]
+imposter = randint(0,len(numlist)-1)
+outlist.append([numlist.pop(imposter),' imposter ',item,'\n'])
 
 outf = open("out.txt", "w")
 
-for i in range (0,len(numlist))
-  outlist[i+1] = [numlist[i],item]
-  
-f.write(outlist)
-f.close()
+for i in range (0,len(numlist)):
+  outlist.append([numlist[i],' commoner\n'])
+
+for x in range (0,len(outlist)):
+	outf.writelines(outlist[x])
+outf.close()
