63f48cb2f96c79e38e83c3e1396a2697cfde1cc9
jo <djh1997@hotmail.com>
2026-07-23 13:58:14 +0100

    improve

--- bingo/bingo.js
+++ bingo/bingo.js
@@ -1,7 +1,9 @@
 function PopRandElm(arr){
+
   if (arr.length === 0 ) return undefined;
   const index = Math.floor(Math.random()*arr.length);
   return arr.splice(index,1)[0];
+
 }
 
 function BuildBingo(){
@@ -9,9 +11,19 @@ function BuildBingo(){
   var form = document.getElementById("frm1");
   var input = form.elements.input.value.trim();
   var output = document.getElementById("output");
-
-
   var inputs = input.split(/,/).filter(Boolean);
-  output.innerHTML = PopRandElm(inputs)
+  var BingoTable = '<table style="width:100%; height:900px;">';
+  for (var i = 0; i < 5; i++) {
+    BingoTable += '<tr>';
+    for (var x = 0; x < 5; x++) {
+      if (i === 2 && x === 2) {
+        BingoTable += '<td>Free<p>Bingo<p>Space</td>';
+        continue;
+      }
+      BingoTable += '<td>' + PopRandElm(inputs) + '</td>';
+    }
+    BingoTable += '</tr>';
+  }
+  output.innerHTML = BingoTable
 
 }
