e51d48aedf439105b94f852f54058ac831dfd889
jo <jo@hawkins.engineer>
2026-07-30 15:02:27 +0100

    Enhance SelectWord with actual word list
    Updated the SelectWord function to include a larger set of words.

--- imposter/imposter.js
+++ imposter/imposter.js
@@ -49,7 +49,7 @@ function DisplayRole(name){
 }
 
 function SelectWord(){
-  var words = '1,2,3,4,5,6,7,8,9,0'
+  var words = 'Elephant,Penguin,Octopus,Kangaroo,Giraffe,Dolphin,Peacock,Hedgehog,Koala,Owl,Camel,Cheetah,Lobster,Polar Bear,Bat,Pizza,Ice Cream,Sushi,Pancake,Watermelon,Avocado,Chocolate,Coffee,Popcorn,Donut,Cheeseburger,Spaghetti,Lemon,Carrot,Milkshake,Toothbrush,Vacuum Cleaner,Lamp,Mirror,Pillow,Microwave,Refrigerator,Scissors,Umbrella,Alarm Clock,Soap,Washing Machine,Kettle,Doorbell,Blanket,Smartphone,Laptop,Headphones,Printer,Robot,Drone,Calculator,Keyboard,Satellite,Video Game,USB Drive,Smartwatch,Webcam,Wi‑Fi,Battery,Airport,Library,Hospital,Zoo,Beach,Castle,Museum,Supermarket,School,Stadium,Cinema,Train Station,Lighthouse,Desert,Waterfall,Doctor,Teacher,Firefighter,Pilot,Chef,Detective,Farmer,Astronaut,Musician,Electrician,Football,Tennis,Swimming,Cycling,Skiing,Golf,Archery,Chess,Fishing,Marathon,Eiffel Tower,Great Wall of China,Moon,Rainbow,Volcano,Pyramid,Mount Everest,Northern Lights,Solar Eclipse,Statue of Liberty,Bicycle,Helicopter,Submarine,Motorcycle,Rocket,Hot Air Balloon,Skateboard,Ferry,Ambulance,Tractor,Tree,Flower,Ocean,Tornado,Glacier,River,Jungle,Coral Reef,Cave,Thunderstorm,Guitar,Movie,Comic Book,Magic Trick,Puzzle,Orchestra,Podcast,Camera,Board Game,Karaoke,Shadow,Time Machine,Treasure Map,Magnet,Compass,Skeleton,Diamond,Fingerprint,Robot Vacuum,Telescope,Secret Door,Snowman,Backpack,Volcano,Lighthouse,Gravity,Democracy,Memory,Echo,Electricity,Inflation,Luck,Jealousy,Reputation,Tradition'
   words = words.split(/,/).filter(Boolean);
   if (words.length === 0 ) return undefined;
   const index = Math.floor(Math.random()*words.length);
